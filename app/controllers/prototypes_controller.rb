8819244411fe8f046c2206fa9441f5e76bddb130
大橋　広記 <hirokiohashi19820409@gmail.com>
2021-05-26 20:27:17 +0900

    move_to_indexの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index,]
-  before_action :move_to_index, except: [:show, :index,]
+  before_action :move_to_index, except: [:show, :index, :new, :create]
   
   def index
     @prototypes = Prototype.all
@@ -52,7 +52,7 @@ class PrototypesController < ApplicationController
 
   def move_to_index
     @prototype = Prototype.find(params[:id])
-    unless @prototype.user == current_user.id
+    unless @prototype.user_id == current_user.id
       redirect_to action: :index
     end
   end

9754995ec3e2602a8376efef377e5d467d214147
大橋　広記 <hirokiohashi19820409@gmail.com>
2021-05-29 22:00:11 +0900

    modification_two

--- db/schema.rb
+++ db/schema.rb
@@ -47,9 +47,10 @@ ActiveRecord::Schema.define(version: 2021_05_23_003816) do
     t.string "title", default: "", null: false
     t.text "catch_copy", null: false
     t.text "concept", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.index ["user_id"], name: "index_prototypes_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -71,4 +72,5 @@ ActiveRecord::Schema.define(version: 2021_05_23_003816) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "prototypes"
   add_foreign_key "comments", "users"
+  add_foreign_key "prototypes", "users"
 end

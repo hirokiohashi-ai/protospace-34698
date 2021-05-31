2990b6fbf044982545717a2b255161ea797e5518
大橋　広記 <hirokiohashi19820409@gmail.com>
2021-06-01 08:54:39 +0900

    modification_five

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ApplicationRecord
   belongs_to :user
-  has_many :comments  # commentsテーブルとのアソシエーション
+  has_many :comments, dependent: :destroy   # commentsテーブルとのアソシエーション
   has_one_attached :image
   validates :image, presence: true
   validates :title, presence: true
